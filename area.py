1977c8e1d55a85d48fbed63f96e2b02ada613904
Babar <babartuhin@gmail.com>
2022-02-02 02:47:25 +0600

    python module creating

---
+++ area.py
@@ -0,0 +1,6 @@
+def triangle_area(b,h):
+    print(f"Area of triangle : {0.5*b*h}")
+
+def rectangle_area(a,b):
+    print(f"Area of rectangle : {a*b}")
+
---
+++ module creating .py
@@ -0,0 +1,4 @@
+from area import rectangle_area,triangle_area
+
+triangle_area(20,25)
+rectangle_area(15,5)
