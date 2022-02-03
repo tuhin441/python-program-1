2c13b3098d23e001ca5b943b3d264cc97ef2d406
Babar <babartuhin@gmail.com>
2022-02-03 08:41:15 +0600

    same function used for different purpose

---
+++ polymorphism.py
@@ -0,0 +1,11 @@
+#builin Polymorphic function
+print(len("Anisul Islam"))
+print(len([10,20,30]))
+
+#user difined polimorphic function
+
+def add(x,y,z=0):
+    return x+y+z
+
+print(add(2,3))
+print(add(2,3,4))
